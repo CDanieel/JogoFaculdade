a5df2fa5c8ac2c360b20b53af1c840eccfca0d67
C.Daniel <csgoimpress2018@gmail.com>
2024-03-25 16:58:04 -0300

    Signed-off-by: C.Daniel <csgoimpress2018@gmail.com>

---
+++ DiagramaClasseJogo/Game.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Game:
+    def __init__(self):
+        self.window_Surface = None
+        self.Part1 = None
+
+    def run(self, ):
+        pass
+
---
+++ DiagramaClasseJogo/Menu.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Menu:
+    def __init__(self):
+        self.window_Surface = None
+        self.Attribute1 = None
+
+    def run(self, ):
+        pass
+
